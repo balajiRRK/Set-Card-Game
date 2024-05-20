215203bbbf8c496e418351769289751defbf3436
Roundtree277 <169216679+Roundtree277@users.noreply.github.com>
2024-05-19 20:26:57 -0400

    Completed the rest of the functions in StopWatch.js, added tests for the rests of the functions in timetest.html  and added comments to each function.

--- StopWatch.js
+++ StopWatch.js
@@ -1,9 +1,15 @@
+/* 
+Stopwatch object that takes the parameters step which is whether the timer will increment or increment and duration if there is a time limit for decrements.
+*/
 function Stopwatch(step, duration){
     this.step =step;
-    this.duration = duration;
+    this.duration = duration; // duration is in seconds.
     this.time=0;
     this.test=0;
     
+    /*
+    If the timer is incrementing then step is +, otherwise  it is decrementing.
+    */
     if(step=="+"){
        incInterval(this);
        
@@ -15,32 +21,38 @@ function Stopwatch(step, duration){
     }
     
 }
-
+/*
+Increments the timer up by one, takes the Stopwatch object as a parameter.
+*/
 function incInterval(Stopwatch){
     // let initial = Stopwatch.time;
-    Stopwatch.Interval= setInterval(function(){Stopwatch.time+=1},1000);
-    console.log("the id is "+ Stopwatch.Interval);
+    Stopwatch.Interval= setInterval(function(){Stopwatch.time+=1},1000); // 1000 miliseconds as that is equal to one second.
+    console.log("the id is "+ Stopwatch.Interval); // for debugging 
 }
-
+/*
+decrements the timer up by one, takes the Stopwatch object as a parameter.
+*/
 function decInterval(Stopwatch, duration ){
     Stopwatch.time=duration;
     // let initial = Stopwatch.time;
-    return setInterval(function(){Stopwatch.time-=1},1000);
+    Stopwatch.Interval= setInterval(function(){Stopwatch.time-=1},1000); // settimeout is easier to implement for decrement.
 }
-
+/* erases the time and stops the interval */
 function clearTime(Stopwatch){
-    console.log("Clearing interval with id "+Stopwatch.Interval);
+    console.log("Clearing interval with id "+Stopwatch.Interval); // for debugging
     clearInterval(Stopwatch.Interval);
     Stopwatch.time=0;
     
 }
+/* stops the interval but keeps the time */
 function pauseTime(Stopwatch){
+    console.log("Pausing interval with id "+Stopwatch.Interval); // for debugging
     clearInterval(Stopwatch.Interval);
 }
+/* resumes the time after a pause */
 function resumeIncTime(Stopwatch){
-    return setInterval(Stopwatch.time+=1,1000);
+    Stopwatch.Interval= setInterval(function(){Stopwatch.time+=1},1000);
 }
-function resumeIncTime(Stopwatch){
-    return setInterval(Stopwatch.time-=1,1000);
-}
+/* resumeDecTime was removed because I do not think it is needed for the game*/
+
 
