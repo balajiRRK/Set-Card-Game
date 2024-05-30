81c52678bdfc960e7dc2e3528f30dc53b75e8fb2
Shristi Keshri <shristikeshri.21@gmail.com>
2024-05-30 19:15:01 -0400

    merged the card functions that Ryan did.

--- Cards.js
+++ Cards.js
@@ -176,3 +176,27 @@ const card6 = { color: 'green', number: 1 };
 
 console.log(checkColor(card4, card5, card6)); // true (all colors are different)
 console.log(checkNumber(card4, card5, card6)); // true (all numbers are the same)
+
+// the function checks three cards shading and compare whther they are all same or all different
+ function checkShading(card1, card2, card3) {
+  //store the shape value of each card in an array
+  const shading = [card1.shape, card2.shape, card3.shape];
+  //check whether the shape of the three cards are same
+  const allSame =  (shading[0] == shading[1]) &&  (shading[0] == shading[2]) &&  (shading[1] == shading[2]);
+  //check whether the shape of the three cards are different
+  const allDifferent =  (shading[0] != shading[1]) &&  (shading[0] != shading[2]) &&  (shading[1] != shading[2]);
+  //return true if either the shading are same or are different
+  return allSame || allDifferent;
+}
+
+// the function checks three cards shape and compare whther they are all same or all different.
+ function checkShapes(card1, card2, card3) {
+  //store the shape value of each card in an array.
+  const shapes = [card1.shape, card2.shape, card3.shape];
+  //check whether the shape of the three cards are same.
+  const allSame = (shapes[0] == shapes[1]) && (shapes[0] == shapes[2]) && (shapes[1] == shapes[2]);
+  //check whether the shape of the three cards are different.
+  const allDifferent = (shapes[0] != shapes[1]) && (shapes[0] != shapes[2]) && (shapes[1] != shapes[2]);
+  //return true if either the shapes are same or are different.
+  return allSame || allDifferent;
+}
