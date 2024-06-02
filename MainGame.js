4535ea8a9a5f113ddd0e1cf73567426c6886f2b4
Balaji Radhakrishnan <rbalajirrk@gmail.comm>
2024-06-01 23:32:27 -0400

    Removed multiplayer related code and fixed the instruction button in mainPageLayout.html to work.

--- MainGame.js
+++ MainGame.js
@@ -1,23 +1,15 @@
-var scoreboard;
-const playerKeys = ['A', 'L', 'H', 'K'];
-
-// creating the players
-let playerList = [];
 let player =1;
 let set = [];
 let click =0;
 
-// addPlayerToGame(player,playerList);
-// initializeScore();
-diffultyMode("normal");
+difficultyMode("normal");
 
 // gneration of the cards
-let cards =  generateCardsWithSets2(5,81,12);//dealCardsWithoutDuplicates(81);//dealCardsWithoutDuplicates(81);//generateCardsWithSets(1,81);//dealCardsWithoutDuplicates(81);
+let cards = generateCardsWithSets2(5,81,12);//dealCardsWithoutDuplicates(81);//dealCardsWithoutDuplicates(81);//generateCardsWithSets(1,81);//dealCardsWithoutDuplicates(81);
 
 let cardsInPlay = cards.slice(0,12);
 
-
-let i =0;
+let i = 0;
 
 while (i<cardsInPlay.length){
    
@@ -32,37 +24,6 @@ while (i<cardsInPlay.length){
     i++;    
     card.onclick = () => cellClicked(can.item(0),set);
 }
-
-function addPlayerToGame(numOfPlayers,playerList){
-    while(playerList.length<numOfPlayers ){
-    addToPlayerList(numOfPlayers, playerList);
-    }
-}
-
-function initializeScore(){
-    //scoreboard = new Array(playerList.length).fill(0);
-
-    const urlParams = new URLSearchParams(window.location.search);
-    const playerNum = parseInt(urlParams.get('playerNum'));
-    playerScoresContainer.innerHTML = '';
-
-    //const playerScoresContainer = document.getElementById('player-scores');
-    // Clear any existing content
-
-    // Loop through the players array and create a list item for each player
-    /*for(let i = 0; i < playerNum; i ++){
-        const listItem = document.createElement('li');
-        listItem.textContent = `${player}: 0`;
-        playerScoresContainer.appendChild(listItem);
-    } */
-
-    playerNum.forEach(player => {
-        const listItem = document.createElement('li');
-        listItem.textContent = `${player}: 0`;
-        playerScoresContainer.appendChild(listItem);
-    });
-}
-
 
 var stopwatch;
 
@@ -86,14 +47,7 @@ document.addEventListener('GameStop', function(e){
     pauseTimer(stopwatch);
 })
 
-document.addEventListener('keyup', (e) => {
-    var playerIndex = playerKeys.indexOf(e.code);
-    if (playerIndex !== -1) {
-        scoreKeeping(scoreboard, playerIndex); 
-    }
-});
-
-function diffultyMode(difficulty){
+function difficultyMode(difficulty){
     const gameGrid = document.getElementById('game-grid');
 
     let numberOfCards;
@@ -121,6 +75,7 @@ function diffultyMode(difficulty){
         gameGrid.style.gridTemplateColumns = 'repeat(6, 1fr)';
     }
 }
+
 function cellClicked(card,set){
     setCard= reforgeCard(card);
     
@@ -148,6 +103,7 @@ function cellClicked(card,set){
     }
 
 }
+
 function isInSet(set,card){
     let inSet =false;
     // console.log("isInSet test");
@@ -159,6 +115,7 @@ function isInSet(set,card){
     // console.log("end of isInSet test");
     return inSet;
 }
+
 function reforgeCard(card){
     let forgedCard = createRandomCard();
     forgedCard.color =card.getAttribute("color");
@@ -169,6 +126,7 @@ function reforgeCard(card){
     // console.log(equals(forgedCar))
     return forgedCard;
 }
+
 function restartGame(){
     window.location.href = 'home.html';
 }
